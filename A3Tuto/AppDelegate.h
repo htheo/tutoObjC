918c07cdf960eb29c46acba7b7b6f2989bbd4f98
Hinfray Theo <hinfraytheo@gmail.com>
2016-10-31 16:05:36 +0100

    Initial Commit

---
+++ A3Tuto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  A3Tuto
+//
+//  Created by Hinfray Theo on 31/10/16.
+//  Copyright © 2016 Hinfray Theo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ A3Tuto/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  A3Tuto
+//
+//  Created by Hinfray Theo on 31/10/16.
+//  Copyright © 2016 Hinfray Theo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
